0508a59d373ff13d63c27eae8566c29b0b0ec2d1
Pete Turner <pturner1989@gmail.com>
2024-02-19 12:05:35 +0000

    Added shadcn button

--- src/app/routes/[route]/page.tsx
+++ src/app/routes/[route]/page.tsx
@@ -1,24 +1,28 @@
+import { Button } from "@/components/ui/button";
 import getRoutes from "@/data/getRoutes"
 import Link from "next/link";
 
 export async function generateStaticParams() {
     const routes = await getRoutes();
     return routes.map(route => {
-        return {route}
+        return { route }
     })
 }
 
-export default async function Page({params}: {params:{route:string}}) {
+export default async function Page({ params }: { params: { route: string } }) {
     const route = params.route;
     return (
         <div>
-        <h1>{route}</h1>
-        <h2>Downloads</h2>
-        <ul>
-        <Link href={`/api/routes/${params.route}/route.gpx`}>GPX</Link>
-        <Link href={`/api/routes/${params.route}/route.geojson`}>GeoJSON</Link>
-
-        </ul>
+            <h1>{route}</h1>
+            <h2>Download</h2>
+            <div className="flex gap-4">
+                <Button asChild>
+                    <Link href={`/api/routes/${params.route}/route.gpx`}>GPX</Link>
+                </Button>
+                <Button asChild>
+                    <Link href={`/api/routes/${params.route}/route.geojson`}>GeoJSON</Link>
+                </Button>
+            </div>
         </div>
 
     )
