fda31041a4909cf80932112be589b459667ab90f
Pete Turner <pturner1989@gmail.com>
2024-02-19 10:24:30 +0000

    Fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: "/bbug-leisure-routes",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
