30add418c9541ca47c4b55702fc54ae40a674979
Pete <pturner1989@gmail.com>
2024-02-19 09:45:29 +0000

    Adde link to GPX

--- src/app/routes/[route]/page.tsx
+++ src/app/routes/[route]/page.tsx
@@ -1,4 +1,5 @@
 import getRoutes from "@/data/getRoutes"
+import Link from "next/link";
 
 export async function generateStaticParams() {
     const routes = await getRoutes();
@@ -10,6 +11,10 @@ export async function generateStaticParams() {
 export default async function Page({params}: {params:{route:string}}) {
     const route = params.route;
     return (
+        <div>
         <h1>{route}</h1>
+        <Link href={`/api/routes/${params.route}/route.gpx`}>Download Route</Link>
+        </div>
+
     )
 }
