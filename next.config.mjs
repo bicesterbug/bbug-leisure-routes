63b2e0f9507acda8f451ca827b81a4ab89e8139b
Pete <pturner1989@gmail.com>
2024-09-30 21:10:12 +0100

    switch ot non-static

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    //output: 'export',
     basePath: process.env.NEXT_PUBLIC_BASE_URL ?? '',
-    images: {
+    // images: {
       
-        loader: "custom",
-        imageSizes: [16, 48, 96, 256],
-        deviceSizes: [640, 2048],
-      },
-      transpilePackages: ["next-image-export-optimizer"],
-      env: {
-        nextImageExportOptimizer_imageFolderPath: "public/routes",
-        nextImageExportOptimizer_exportFolderPath: "out",
-        nextImageExportOptimizer_quality: "100",
-        nextImageExportOptimizer_storePicturesInWEBP: "true",
-        nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
+    //     loader: "custom",
+    //     imageSizes: [16, 48, 96, 256],
+    //     deviceSizes: [640, 2048],
+    //   },
+    //  transpilePackages: ["next-image-export-optimizer"],
+      // env: {
+      //   nextImageExportOptimizer_imageFolderPath: "public/routes",
+      //   nextImageExportOptimizer_exportFolderPath: "out",
+      //   nextImageExportOptimizer_quality: "100",
+      //   nextImageExportOptimizer_storePicturesInWEBP: "true",
+      //   nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
     
-        // If you do not want to use blurry placeholder images, then you can set
-        // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
-        // `placeholder="empty"` to all <ExportedImage> components.
-        nextImageExportOptimizer_generateAndUseBlurImages: "true",
+      //   // If you do not want to use blurry placeholder images, then you can set
+      //   // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+      //   // `placeholder="empty"` to all <ExportedImage> components.
+      //   nextImageExportOptimizer_generateAndUseBlurImages: "true",
     
-        // If you want to cache the remote images, you can set the time to live of the cache in seconds.
-        // The default value is 0 seconds.
-        nextImageExportOptimizer_remoteImageCacheTTL: "0",
-      },
+      //   // If you want to cache the remote images, you can set the time to live of the cache in seconds.
+      //   // The default value is 0 seconds.
+      //   nextImageExportOptimizer_remoteImageCacheTTL: "0",
+      // },
 };
 
 export default nextConfig;
